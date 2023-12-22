18e1ab04213d7c15b0a98a1fb287459aed21b265
Arunram2000 <70103361+Arunram2000@users.noreply.github.com>
2023-12-22 18:31:26 +0530

    chore: otp method updated

--- src/index.ts
+++ src/index.ts
@@ -28,65 +28,57 @@ const SECRET_KEY = process.env.STRIPE_SECRET_KEY || "default_secret_key";
 
 const stripe = new Stripe(SECRET_KEY);
 
-app.post(
-  "/webhooks",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    console.log("called");
-    const sig = req.headers["stripe-signature"];
-    const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+app.post("/webhooks", express.raw({ type: "application/json" }), async (req, res) => {
+  console.log("called");
+  const sig = req.headers["stripe-signature"];
+  const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-    console.log("req.body:", req.body);
-    console.log("sig:", sig);
-    console.log("stripeWebhookSecret:", stripeWebhookSecret);
+  console.log("req.body:", req.body);
+  console.log("sig:", sig);
+  console.log("stripeWebhookSecret:", stripeWebhookSecret);
 
-    if (typeof sig !== "string") {
-      res.status(400).json({ message: "Bad Request" });
-      return;
-    }
-
-    if (typeof stripeWebhookSecret !== "string" || stripeWebhookSecret === "") {
-      console.error("Stripe webhook secret is not defined or empty.");
-      res.status(500).json({ message: "Internal server error" });
-      return;
-    }
-
-    let event;
-    let payload;
-
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        stripeWebhookSecret
-      );
-    } catch (err: any) {
-      payload = JSON.parse(err.payload);
-
-      console.error(err);
-      // res.status(400).json({ message: "Bad Request" });
-      // return;
-    }
-
-    if (payload.type === "payment_intent.created") {
-      console.log(`${payload.data.object.metadata.name} initated payment!`);
-    }
-    if (payload.type === "payment_intent.succeeded") {
-      console.log(payload.data);
-      console.log(`${payload.data.object.metadata.name} succeeded payment!`);
-      try {
-        await db
-          .collection("Orders")
-          .doc(payload.data.object.id)
-          .update({ paymentStatus: "SUCCESS" });
-      } catch (error) {
-        console.log("ERROR ON STROING DB", error);
-      }
-      console.log(`updaetd on db`);
-    }
-    res.json({ ok: true });
+  if (typeof sig !== "string") {
+    res.status(400).json({ message: "Bad Request" });
+    return;
   }
-);
+
+  if (typeof stripeWebhookSecret !== "string" || stripeWebhookSecret === "") {
+    console.error("Stripe webhook secret is not defined or empty.");
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
+
+  let event;
+  let payload;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, stripeWebhookSecret);
+  } catch (err: any) {
+    payload = JSON.parse(err.payload);
+
+    console.error(err);
+    // res.status(400).json({ message: "Bad Request" });
+    // return;
+  }
+
+  if (payload.type === "payment_intent.created") {
+    console.log(`${payload.data.object.metadata.name} initated payment!`);
+  }
+  if (payload.type === "payment_intent.succeeded") {
+    console.log(payload.data);
+    console.log(`${payload.data.object.metadata.name} succeeded payment!`);
+    try {
+      await db
+        .collection("Orders")
+        .doc(payload.data.object.id)
+        .update({ paymentStatus: "SUCCESS" });
+    } catch (error) {
+      console.log("ERROR ON STROING DB", error);
+    }
+    console.log(`updaetd on db`);
+  }
+  res.json({ ok: true });
+});
 
 app.use(express.json({}));
 
@@ -180,73 +172,21 @@ app.post("/test", async (req, res) => {
   }
 });
 
-// app.post("/create-payment-intent", async (req, res) => {
-//   try {
-//     const { email, name, currency, amount } = req.body;
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: Math.round(amount * 100),
-//       currency: currency,
-//       payment_method_types: ["card"],
-//       // automatic_payment_methods: {
-//       //   enabled: true,
-//       // },
-//       metadata: {
-//         email,
-//         name,
-//         currency,
-//         amount,
-//       },
-//     });
-//     console.log(paymentIntent.status);
-
-//     if (paymentIntent.status === "succeeded") {
-//       // Payment was successful
-//       console.log("success");
-//     } else if (paymentIntent.status === "requires_payment_method") {
-//       // Handle payment method error here
-//       console.log(paymentIntent.last_payment_error);
-//       const error = paymentIntent.last_payment_error;
-//       if (error) {
-//         if (error.code === "insufficient_funds") {
-//           // Handle insufficient funds error
-//           console.error("Insufficient Funds Error");
-//           // Display an error message to the user
-//           // You can use an alert or update your UI to inform the user about the issue
-//         } else {
-//           // Handle other payment method errors
-//           console.error("Payment Method Error:", error.message);
-//           // Display a relevant error message to the user
-//         }
-//       }
-//     }
-//     const clientSecret = paymentIntent.client_secret;
-//     console.log(paymentIntent.id);
-//     res.json({
-//       message: "Payment initiated",
-//       amount: amount,
-//       email: email,
-//       name: name,
-//       currency: currency,
-//       clientSecret,
-//       paymentId: paymentIntent.client_secret,
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     //@ts-ignore
-//     res.status(500).json({ message: `Internal server error : ${err}` });
-//   }
-// });
-
 app.post("/send-otp", async (req, res) => {
+  const phoneNumber = req.body.phoneNumber;
+  const otp = req.body.otp;
+
+  if (!phoneNumber || !otp) return res.status(403).json({ error: { message: "invalid values" } });
+
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = new Twilio(accountSid, authToken);
 
   try {
     const message = await client.messages.create({
-      body: "You have an appointment with Owl, Inc. on Friday, November 3 at 4:00 PM. Reply C to confirm.",
-      to: "+917358247659",
-      from: "+18582810964",
+      body: `Dear customer, use this One Time Password ${otp} to log in to your SprinkleNadar App. This OTP will be valid for the next 5 mins.`,
+      from: "+17404956025",
+      to: phoneNumber,
     });
 
     res.json(message);
@@ -393,9 +333,7 @@ app.post("/pushToken", async (req, res) => {
     //       console.error("Error sending message:", error);
     //     });
     // }
-    for (let apnPushToken of [
-      "eadc9d5083ce3c99a1817f6fde00d9b75cf899f28322bf4482425eb350d5f690",
-    ]) {
+    for (let apnPushToken of ["eadc9d5083ce3c99a1817f6fde00d9b75cf899f28322bf4482425eb350d5f690"]) {
       console.log(path.join(__dirname, "./apnkey.p8"));
       const apnKey = path.join(__dirname, "./apnkey.p8");
 
